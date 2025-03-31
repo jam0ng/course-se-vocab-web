8abab1a8160e94124c5c860f684096797ef6b633
hyungjun0312 <156741635+hyungjun0312@users.noreply.github.com>
2025-03-31 15:40:14 +0900

    Add files via upload

--- SoftwareEngineering/backend/models/Word.js
+++ SoftwareEngineering/backend/models/Word.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const WordSchema = new mongoose.Schema({
   english: { type: String, required: true },
   korean: { type: String, required: true },
-  example: { type: String },
-  cloze: { type: String }
+  example: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Word', WordSchema);
